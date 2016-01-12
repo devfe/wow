de4c572a2058d8d5586d4ee9ec4d3074b56ea1aa
zhishaofei <zhishaofei@jd.com>
2016-01-12 10:41:25 +0800

    sprite输出png8图片

--- gulp/sprite.js
+++ gulp/sprite.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var data = require('gulp-data');
 var spritesmith = require('gulp.spritesmith');
+var rename = require("gulp-rename");
+var gulpFilter = require('gulp-filter');
+var imageminPngquant = require('imagemin-pngquant');
+var buffer = require('vinyl-buffer');
+
+var path = require('path');
 
 module.exports = function (config) {
     var sprite = config.sprite;
@@ -7,11 +14,21 @@ module.exports = function (config) {
     return function (cb) {
         cb = cb || function() {};
 
+        var pngFilter = gulpFilter(['**/*.png']);
+
         gulp.src(sprite.src)
             .pipe(spritesmith({
                 imgName: sprite.imgName,
                 cssName: sprite.cssName
             }))
+            .pipe(gulp.dest(sprite.dest))
+            .pipe(pngFilter)
+            .pipe(buffer())
+            .pipe(rename(function (path) {
+                path.basename += '_8';
+                path.extname = '.png'
+            }))
+            .pipe(imageminPngquant()())
             .pipe(gulp.dest(sprite.dest))
             .on('end', cb);
     }
